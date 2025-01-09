0632bb076229bb88281a3ea267e823ee44e22cf5
turnsy <x2019cqr@stfx.ca>
2025-01-08 23:37:30 -0400

    fix: cleans up unused html generator

--- src/main/java/com/chatroom/HTMLFactory.java
+++ src/main/java/com/chatroom/HTMLFactory.java
@@ -10,21 +10,6 @@ public class HTMLFactory {
     public HTMLFactory() {
     }
 
-    public String getUsersHTML(Collection<String> users) {
-        StringBuilder HTMLBuilder = new StringBuilder();
-        HTMLBuilder.append(
-                """
-                        <div hx-swap-oob="beforeend:#connect_status">
-                          <h6>Connected users:</h6>
-                        """);
-        users.forEach((user) -> {
-            HTMLBuilder.append("<p>").append(user).append("</p>");
-        });
-        HTMLBuilder.append("</div>");
-
-        return HTMLBuilder.toString();
-    }
-
     public String getMessageHTML(String message, String username, boolean owned) {
         if (owned) {
             return div(
